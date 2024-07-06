7e968aeeb2b025f7c308b477e070c24ea05a36e4
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-06 11:38:05 -0700

    Added some CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,80 @@
 
-const getTheYear = () => {
-    const newDate = new Date();
-    return newDate.getFullYear();
+// import React from 'react';
+// Another Method
+
+import React, {Component} from 'react';
+
+
+
+
+// const getTheYear = () => {
+//     const newDate = new Date();
+//     return newDate.getFullYear();
+// }
+
+
+
+// Functional Component
+// const Header = () => {
+     
+//     const getTheYear = () => {
+//         const newDate = new Date();
+//         return newDate.getFullYear();
+//     }
+
+//     return(
+//         <>
+//             <div>
+//                 The Date is {getTheYear()}
+//             </div>
+//         </>
+//     )
+
+
+// }
+
+
+
+// Adding Styles
+// This is the one way of adding css
+// let styles = {
+//     header:{
+//         background:'red'
+//     },
+//     logo:{
+//         color:'blue',
+//         fontFamily:'Anton',
+//         textAlign:'center'
+//     }
+// }
+
+
+// Class Component
+// class Header extends React.Component{
+// Another Method
+class Header extends Component{
+     
+    render(){
+        return(
+            <>
+                {/* <div style={styles.header}>
+                   <div style={styles.logo}>Logo</div>
+                   <input/>
+                </div> */}
+
+                <header>
+                    <div className='logo'>Logo</div>
+                    <input/>
+                </header>
+
+
+            </>
+        )
+    }
+
+
 }
 
 
-
-
-const Header = () => (
-    <>
-        <div>
-            The Date is {getTheYear()}
-       </div>
-    
-    </>
-)
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 
+// This is the 3rd way of adding css
+import './styles/styles.css';
+
 const App = () => {
     return(
         <div className='salam'>
-            <h1>
+            {/* <h1>
                 Assalam O Alaikom!!
-            </h1>
+            </h1> */}
 
             <Header/>
 
